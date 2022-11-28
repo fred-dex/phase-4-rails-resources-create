076a13d986f2d3e996abfc8da602e916a109eb43
Fred Dex <fredjuiceman@yahoo.com>
2022-11-28 14:50:15 -0500

    commit tree ba9aa651dcaf455c11d14eca18f6d7c2746ed301

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,10 @@
 class BirdsController < ApplicationController
 
+  def create
+    bird = Bird.create(name: params, species: params)
+  render json: bird, status: :created
+  end 
+
   # GET /birds
   def index
     birds = Bird.all
